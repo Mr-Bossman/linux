7d40037c8cb357beac2a8cc189b5da80f2eca397
Jesse Taube <jesse@rivosinc.com>
2025-08-05 09:19:27 -0700

    fix sudo modprobe data_breakpoint ksym=nr_threads

--- samples/hw_breakpoint/data_breakpoint.c
+++ samples/hw_breakpoint/data_breakpoint.c
@@ -23,10 +23,10 @@
 
 static struct perf_event * __percpu *sample_hbp;
 
-static char ksym_name[KSYM_NAME_LEN] = "jiffies";
+static char ksym_name[KSYM_NAME_LEN] = "nr_threads";
 module_param_string(ksym, ksym_name, KSYM_NAME_LEN, S_IRUGO);
-MODULE_PARM_DESC(ksym, "Kernel symbol to monitor; this module will report any"
-			" write operations on the kernel symbol");
+MODULE_PARM_DESC(ksym, "GPL-only Kernel symbol to monitor; this module will"
+		       "report any write operations on the kernel symbol");
 
 static void sample_hbp_handler(struct perf_event *bp,
 			       struct perf_sample_data *data,
